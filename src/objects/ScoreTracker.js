177af6519d116a23c42ef664dfbad1d816591ec3
Jean M. Lescure <jean@ticowebmedia.com>
2017-05-14 21:09:20 -0600

    Closes #39: I should see the score

--- src/objects/ScoreTracker.js
+++ src/objects/ScoreTracker.js
@@ -13,7 +13,12 @@ export default class ScoreTracker extends Phaser.Group {
   constructor({game, parent, name}) {
     super(game, parent, name);
 
+    this.goal = this.game.world.store.match.goal;
+    this.hvGoal = this.goal / 2;
+    this.threeQtGoal = (this.goal / 4) * 3;
+
     this.addGoalText();
+    this.addScoreText();
   }
 
   /**
@@ -37,13 +42,40 @@ export default class ScoreTracker extends Phaser.Group {
   }
 
   /**
+   * Adds the score text.
+   */
+  addScoreText() {
+    const scoreTextArgs = [
+      this.game,
+      5,
+      84,
+      `Spent: $${this.game.world.store.match.score}`,
+      {
+        font: 'normal 18px PT Mono',
+        fill: '#ffffff',
+        stroke: 0x000000,
+        strokeThickness: 4,
+      },
+    ];
+    this.scoreText = new Phaser.Text(...scoreTextArgs);
+    this.game.add.existing(this.scoreText);
+  }
+
+  /**
    * To be called by the parent state's `update()` method.
    */
   updateCallback() {
-    const goal = this.game.world.store.match.goal;
     const score = this.game.world.store.match.score;
-    if (goal < score) {
-      this.goalText.fill = 0xE50000;
+    if (this.hvGoal < score) {
+      this.scoreText.fill = '#f5e500';
     }
+    if (this.threeQtGoal < score) {
+      this.scoreText.fill = '#e5a500';
+    }
+    if (this.goal < score) {
+      this.scoreText.fill = '#e50000';
+    }
+
+    this.scoreText.text = `Spent: $${score}`;
   }
 }
